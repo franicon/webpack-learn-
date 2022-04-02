9dec2201884bc1a4cc0ce0f93183144fb2deea92
Abayomi Aluko <franicons@gmail.com>
2022-04-02 12:39:28 +0100

    feat(added asset modules): add asset modules to add an image to the project
    feature/asset-modules-#2

---
+++ src/add-img.js
@@ -0,0 +1,12 @@
+function Addimage() {
+    
+}
+Addimage.prototype.newImage = function (image) {
+    let img = document.createElement('img');
+    img.width = 300;
+    img.src = image
+    let body = document.querySelector('body');
+    body.appendChild(img);
+}
+
+export default Addimage;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import pic1 from "./game-1.jpg";
 import Greet from "./hello-world.js";
+import Addimage from "./add-img.js";
 
 let sayHelloWord = new Greet();
 sayHelloWord.helloWorld();
 
 let hellofolks = new Greet();
 hellofolks.helloFolks();
+
+let addImage = new Addimage();
+addImage.newImage(pic1);
+
+
